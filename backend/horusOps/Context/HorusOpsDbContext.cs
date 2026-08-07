3baffc7a53bec4c6deea278c205fab65abb26bac
PabloSnhz <pablosanhuezaaa@gmail.com>
2026-08-07 19:25:11 -0300

    feat: conexion a base de datos

---
+++ backend/horusOps/Context/HorusOpsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using horusOps.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace horusOps.Context
+{
+    public class HorusOpsDbContext: DbContext
+    {
+        public HorusOpsDbContext(DbContextOptions<HorusOpsDbContext> options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Empleado> Empleados => Set<Empleado>();
+    }
+}
---
+++ backend/horusOps/Program.cs
@@ -0,0 +1,32 @@
+using horusOps.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<HorusOpsDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("HorusOpsConnection")
+));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
