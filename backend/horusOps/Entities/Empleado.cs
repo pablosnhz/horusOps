25f8f6547746ea018c979f9d611e17afa4c7d4c0
PabloSnhz <pablosanhuezaaa@gmail.com>
2026-08-11 18:13:45 -0300

    entidades-iniciales

---
+++ backend/horusOps/Entities/Empleado.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace horusOps.Entities;
+
+    [Table("EMPLEADOS")]
+    public class Empleado
+{
+    [Key]
+    [Column("ID_EMPLEADO")]
+    public int IdEmpleado { get; set; }
+
+    [Column("NOMBRE_EMPLEADO")]
+    public string NombreEmpleado { get; set; } = string.Empty;
+
+    [Column("APELLIDO_EMPLEADO")]
+    public string ApellidoEmpleado { get; set; } = string.Empty;
+
+    [Column("DNI_EMPLEADO")]
+    public string DniEmpleado { get; set; } = string.Empty;
+
+    [Column("EMAIL_EMPLEADO")]
+    public string EmailEmpleado { get; set;  } = string.Empty;
+
+    [Column("TELEFONO_EMPLEADO")]
+    public string? TelefonoEmpleado { get; set; }
+
+    [Column("FECHA_INGRESO")]
+    public DateTime FechaIngreso { get; set; }
+
+    [Column("ID_SUCURSAL")]
+    public int IdSucursal { get; set; }
+
+    [Column("ACTIVO")]
+    public bool Activo { get; set; }
+
+    [Column("FECHA_ALTA")]
+    public DateTime FechaAlta { get; set; }
+
+    [ForeignKey(nameof(IdSucursal))]
+    public Sucursal Sucursal { get; set; } = null!;
+
+    public ICollection<Venta> Ventas { get; set; } = new List<Venta>();
+}
